b7125bac984d8af35e7da614ee989069bcf29c42
junyeongjjjjj <162803915+junyeongjjjjj@users.noreply.github.com>
2024-11-04 18:59:04 +0900

    9주차 수업

--- week9/first_p5_project/sketch.js
+++ week9/first_p5_project/sketch.js
@@ -1,8 +1,29 @@
+let tilenumX = 16;
+let tilenumY = 12;
+
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 100);
+  background('black');
+  noStroke();
+  fill('cornflowerblue');
+  // for (let column = 0; column < width; column += 40) {
+  //   for (let row = 0; row < height; row += 40) {
+  //     let x = 20 + column;
+  //     let y = 20 + row;
+  //     let diameter = 30;
+  //     circle(x, y, diameter);
+  //   }
+  // }
+  for (let row = 0; row < tilenumY; row++) {
+    for (let column = 0; column < tilenumX; column++) {
+      let tileW = width / tilenumX;
+      let tileH = height / tilenumY;
+      let x = tileW * 0.5 + column * tileW;
+      let y = tileH * 0.5 + row * tileH;
+      ellipse(x, y, tileW, tileH);
+    }
+  }
 }
